49714e9a82b544325f82e56820ba02e388b60522
Kyle Goetke <kgoetke@ucvts.tec.nj.us>
2020-02-10 14:08:33 -0500

    Starting basic sample project

--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module Example {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens sample;
-}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,27 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.button;
+import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+    Button button;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Kyle's Dictionary");
+        primaryStage.setScene(new Scene(root, 960, 540));
+        primaryStage.show();
     }
 }
