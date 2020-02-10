301775eebea7be089fe4c3f062a90c71e7fdf552
Kyle Goetke <kgoetke@ucvts.tec.nj.us>
2020-02-10 14:44:40 -0500

    Working on a basic layout for the GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,13 +4,22 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.button;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
+
+// NOTES TO SELF:
+// Install IntelliJ
+// Download JavaFX 13 from Gluon
+// Import everything into IntelliJ
+// https://www.youtube.com/watch?v=YtKF1JKtRWM&list=PL6gx4Cwl9DGBzfXLWLSYVy8EbTdpGbUIG&index=9
 
 public class Main extends Application {
 
+    Stage window;
     Button button;
 
     public static void main(String[] args) {
@@ -19,9 +28,26 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Kyle's Dictionary");
-        primaryStage.setScene(new Scene(root, 960, 540));
-        primaryStage.show();
+        window = primaryStage;
+        window.setTitle("Window Title");
+        window.setOnCloseRequest(e -> closeProgram());
+        StackPane layout = new StackPane();
+//
+//        HBox leftMenu = new HBox();
+//        Button addButton = new Button("Add");
+//        Button removeButton = new Button("Remove");
+//        leftMenu.getChildren().addAll(addButton, removeButton);
+//        BorderPane leftBorderPane = new BorderPane();
+//        leftBorderPane.setTop(leftMenu);
+
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void closeProgram() {
+        //Boolean answer = ConfirmBox.display("Title", "Are you sure you want to exit?");
+        System.out.println("Safely closed the program");
+        window.close();
     }
 }
