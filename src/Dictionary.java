ec65bc6e38ac63b1b88bb4ff17a9402ce685b2cb
Kyle Goetke <kgoetke@ucvts.tec.nj.us>
2020-02-20 14:21:27 -0500

    Updated Dictionary class

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,11 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
-
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
@@ -13,27 +10,29 @@ import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
 public class Dictionary {
-	public static Words[] wordList;
-	public static void addAllWords() throws JsonSyntaxException, JsonIOException, FileNotFoundException {
-		wordList = new Gson().fromJson(new FileReader(".\\JSON\\words.json"), Words[].class);
-	}
+    public static Words[] wordList;
+    public static void addAllWords() throws JsonSyntaxException, JsonIOException, FileNotFoundException {
+        wordList = new Gson().fromJson(new FileReader("../data/words.json"), Words[].class);
+    }
 
-	public static ArrayList<String> listWords() {
-		ArrayList<String> listOfWords = new ArrayList<String>();
-		for(Words word : wordList) {
-			listOfWords.add(word.getSpelling());
-		}
-		return listOfWords;
-	}
-	private void addWord() {
-		// used to add words
-	}
+    public static ArrayList<String> listWords() {
+        ArrayList<String> listOfWords = new ArrayList<String>();
+        for(Words word : wordList) {
+            listOfWords.add(word.getSpelling());
+        }
+        return listOfWords;
+    }
+    static void addWord() {
+        //TODO implement adding words
+        System.out.println("Add");
+    }
 
-	private void delWord() {
-		// used to delete words
-	}
+    static void removeWord() {
+        //TODO implement deleting words
+        System.out.println("Remove");
+    }
 
-	private void search () {
-		// used to search
-	}
+    private void search () {
+        //TODO implement search
+    }
 }
