2e20f7b19b649080a4baf7a6702b09593f949612
KyleGoetke <goetkek@protonmail.com>
2020-03-01 12:26:50 -0500

    Switching to Swing

--- src/Sorting.java
+++
@@ -1,9 +0,0 @@
-public class Sorting {
-    public static void Ascending() {
-        // sorts words in ascending order
-    }
-
-    public static void Descending() {
-        // sorts words in descending order
-    }
-}
